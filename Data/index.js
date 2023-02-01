07cd72310690d2d6c06746a4afdafc6d1fdfbc06
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-02-01 11:30:48 +0000

    personaje aparece

--- Data/index.js
+++ Data/index.js
@@ -1,1 +1,33 @@
-console.log("JS Online");
+let cuadricula = document.querySelectorAll('.celda')
+let fila = []
+for (let i = 1; i <= 20; i++) {
+    fila.push(document.querySelector(`#row${i}>#col1`))
+}
+// console.log(document.querySelector(`#row3>#col4`));
+
+function Peon(/*vida, balas,*/ x, y) {
+    // this.vida = vida;
+    // this.balas = balas;
+    this.x = x;
+    this.y = y;
+}
+
+Peon.prototype.spwan = function() {
+    getCasilla(this.x, this.y).setAttribute('class', 'soldado');
+}
+
+Peon.prototype.setX = function (x) {
+    this.x = x;
+}
+
+
+Peon.prototype.setY = function (y) {
+    this.y = y;
+}
+
+function getCasilla(row, col) {
+    return document.querySelector(`#row${row}>#col${col}`);
+}
+
+let s = new Peon(5, 1);
+s.spwan()
