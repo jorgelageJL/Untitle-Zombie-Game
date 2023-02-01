b572fd32c28e2c0e98fe60f7b8501725c8e89dbe
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-02-01 14:04:41 +0000

    despues de almuerzo

--- Data/index.js
+++ Data/index.js
@@ -1,55 +1,66 @@
 //VARIABLES
 
-let cuadricula = document.querySelectorAll(".celda");
+// let cuadricula = document.querySelectorAll(".celda");
 let fila = [];
 for (let i = 1; i <= 20; i++) {
-  fila.push(document.querySelector(`#row${i}>#col1`));
+    fila.push(document.querySelector(`#row${i}>#col1`));
 }
 
-// console.log(document.querySelector(`#row3>#col4`));
+console.log(document.querySelector(`#row3>#col4`));
 
 //OBJETO
 
 function Peon(/*vida, balas,*/ x, y) {
-  // this.vida = vida;
-  // this.balas = balas;
-  this.x = x;
-  this.y = y;
-}
-
-Peon.prototype.show = function () {
-  getCasilla(this.x, this.y).setAttribute("class", "soldado");
+    // this.vida = vida;
+    // this.balas = balas;
+    this.x = x;
+    this.y = y;
 };
 
-/*Peon.prototype.hide = function () {
-  getCasilla(this.x, this.y).setAttribute("class", "celda");
-};*/
+Peon.prototype.show = function () {
+    getCasilla(this.x, this.y).setAttribute("class", "soldado");
+};
+
+Peon.prototype.hide = function () {
+    // console.log(this.x, this.y)
+    getCasilla(this.x, this.y).classList.remove('soldado');
+};
 
 Peon.prototype.setPos = function (a) {
-  if (a === "a") this.x--;
-  else if (a === "d") this.x++;
-  else if (a === "w") this.y--;
-  else if (a === "s") this.y++;
+    this.hide();
+    if (a === "a") {
+        this.x--;
+    }
+    else if (a === "d") {
+        this.x++;
+    }
+    else if (a === "w") {
+        this.y--;
+    }
+    else if (a === "s") {
+        this.y++;
+    }
 };
 
 //FUNCIONES SALVAJES
 function getCasilla(col, row) {
-  return document.querySelector(`#row${row}>#col${col}`);
+    return document.querySelector(`#row${row}>#col${col}`);
 }
 let tecla = "d";
 tecla = window.addEventListener("keydown", (e) => {
-  console.log(e.key);
-  s.setPos(e.key);
+    //   console.log(e.key);
+    s.setPos(e.key);
 });
 
 function limpiaCelda() {
-  document.querySelectorAll("td").setAttribute("class", "celda");
+    // document.querySelectorAll("td").setAttribute("class", "celda");
+    s.setPos();
+    s.show();
+    // s.hide();
 }
 
 function loop() {
-  limpiaCelda();
-  s.setPos();
-  s.show();
+    limpiaCelda();
 }
 
 ///LLAMADAS
@@ -57,4 +68,4 @@ let s = new Peon(5, 1);
 
 let timeID = setInterval(loop, 33);
 
-console.log(document.querySelectorAll("td"));
+// console.log(document.querySelectorAll("td"));
