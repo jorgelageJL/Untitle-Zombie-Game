d182cf1e78cf6c976020f8c33268dcaa656a09fe
Arguish <jhergon8@gmail.com>
2023-02-01 14:51:50 +0000

    el zombie se mueve

--- Data/index.js
+++ Data/index.js
@@ -3,7 +3,7 @@
 // let cuadricula = document.querySelectorAll(".celda");
 let fila = [];
 for (let i = 1; i <= 20; i++) {
-    fila.push(document.querySelector(`#row${i}>#col1`));
+  fila.push(document.querySelector(`#row${i}>#col1`));
 }
 
 console.log(document.querySelector(`#row3>#col4`));
@@ -11,84 +11,86 @@ console.log(document.querySelector(`#row3>#col4`));
 //OBJETO
 
 function Peon(/*vida, balas,*/ x, y) {
-    // this.vida = vida;
-    // this.balas = balas;
-    this.x = x;
-    this.y = y;
-};
+  // this.vida = vida;
+  // this.balas = balas;
+  this.x = x;
+  this.y = y;
+}
 
-function Zombie(/*vida, balas,*/ x, y) {
-    // this.vida = vida;
-    // this.balas = balas;
-    this.x = x;
-    this.y = y;
-};
-
+// PEON
 Peon.prototype.show = function () {
-    getCasilla(this.x, this.y).setAttribute("class", "soldado");
+  getCasilla(this.x, this.y).setAttribute("class", "soldado");
 };
 
 Peon.prototype.hide = function () {
-    // console.log(this.x, this.y)
-    getCasilla(this.x, this.y).classList.remove('soldado');
+  // console.log(this.x, this.y)
+  getCasilla(this.x, this.y).classList.remove("soldado");
+};
+Peon.prototype.setPos = function (a) {
+  this.hide();
+  if (a === "a") {
+    this.x--;
+  } else if (a === "d") {
+    this.x++;
+  } else if (a === "w") {
+    this.y--;
+  } else if (a === "s") {
+    this.y++;
+  }
 };
 
+// ZOMBIE
+function Zombie(/*vida, balas,*/ x, y) {
+  // this.vida = vida;
+  // this.balas = balas;
+  this.x = x;
+  this.y = y;
+}
+
 Zombie.prototype.show = function () {
-    getCasilla(this.x, this.y).setAttribute("class", "zombie");
+  getCasilla(this.x, this.y).setAttribute("class", "zombie");
 };
 
 Zombie.prototype.hide = function () {
-    // console.log(this.x, this.y)
-    getCasilla(this.x, this.y).classList.remove('zombie');
+  // console.log(this.x, this.y)
+  getCasilla(this.x, this.y).classList.remove("zombie");
 };
 
-Peon.prototype.setPos = function (a) {
-    this.hide();
-    if (a === "a") {
-        this.x--;
-    }
-    else if (a === "d") {
-        this.x++;
-    }
-    else if (a === "w") {
-        this.y--;
-    }
-    else if (a === "s") {
-        this.y++;
-    }
+Zombie.prototype.move = function () {
+  if (this.x === 1) {
+    this.x = 20;
+  } else {
+    this.x--;
+  }
 };
-
-
 
 //FUNCIONES SALVAJES
 function getCasilla(col, row) {
-    return document.querySelector(`#row${row}>#col${col}`);
+  return document.querySelector(`#row${row}>#col${col}`);
 }
 let tecla = "d";
 tecla = window.addEventListener("keydown", (e) => {
-    //   console.log(e.key);
-    s.setPos(e.key);
+  //   console.log(e.key);
+  s.setPos(e.key);
 });
 
 function limpiaCelda() {
-    // document.querySelectorAll("td").setAttribute("class", "celda");
-    s.setPos();
-    s.show();
-    ene1.show();
-    // s.hide();
+  // document.querySelectorAll("td").setAttribute("class", "celda");
+  s.setPos();
+  s.show();
+  ene1.show();
+  ene1.move();
+  // s.hide();
 }
 
 function loop() {
-    limpiaCelda();
+  limpiaCelda();
 }
 
 ///LLAMADAS
 let s = new Peon(5, 1);
-let ene1 = new Zombie(4, 6)
+let ene1 = new Zombie(4, 6);
 
-let timeID = setInterval(loop, 33);
+let timeID = setInterval(loop, 1000);
 
 // console.log(document.querySelectorAll("td"));
-
-
-
