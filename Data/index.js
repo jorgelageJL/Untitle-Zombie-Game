44b1996366dd5707c6251329fb633a5d88b111da
Arguish <jhergon8@gmail.com>
2023-02-01 12:10:00 +0000

    El machango se mueve pero nose borra

--- Data/index.js
+++ Data/index.js
@@ -1,33 +1,60 @@
-let cuadricula = document.querySelectorAll('.celda')
-let fila = []
+//VARIABLES
+
+let cuadricula = document.querySelectorAll(".celda");
+let fila = [];
 for (let i = 1; i <= 20; i++) {
-    fila.push(document.querySelector(`#row${i}>#col1`))
+  fila.push(document.querySelector(`#row${i}>#col1`));
 }
+
 // console.log(document.querySelector(`#row3>#col4`));
 
+//OBJETO
+
 function Peon(/*vida, balas,*/ x, y) {
-    // this.vida = vida;
-    // this.balas = balas;
-    this.x = x;
-    this.y = y;
+  // this.vida = vida;
+  // this.balas = balas;
+  this.x = x;
+  this.y = y;
 }
 
-Peon.prototype.spwan = function() {
-    getCasilla(this.x, this.y).setAttribute('class', 'soldado');
+Peon.prototype.show = function () {
+  getCasilla(this.x, this.y).setAttribute("class", "soldado");
+};
+
+/*Peon.prototype.hide = function () {
+  getCasilla(this.x, this.y).setAttribute("class", "celda");
+};*/
+
+Peon.prototype.setPos = function (a) {
+  if (a === "a") this.x--;
+  else if (a === "d") this.x++;
+  else if (a === "w") this.y--;
+  else if (a === "s") this.y++;
+};
+
+//FUNCIONES SALVAJES
+function getCasilla(col, row) {
+  return document.querySelector(`#row${row}>#col${col}`);
+}
+let tecla = "d";
+tecla = window.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  s.setPos(e.key);
+});
+
+function limpiaCelda() {
+  document.querySelectorAll("td").setAttribute("class", "celda");
 }
 
-Peon.prototype.setX = function (x) {
-    this.x = x;
+function loop() {
+  limpiaCelda();
+  s.setPos();
+  s.show();
 }
 
-
-Peon.prototype.setY = function (y) {
-    this.y = y;
-}
-
-function getCasilla(row, col) {
-    return document.querySelector(`#row${row}>#col${col}`);
-}
-
+///LLAMADAS
 let s = new Peon(5, 1);
-s.spwan()
+
+let timeID = setInterval(loop, 33);
+
+console.log(document.querySelectorAll("td"));
