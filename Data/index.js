2288ceb6780d63b0dc41c809e45f0f33d81964d8
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-02-01 16:48:34 +0000

    codigo limpio

--- Data/index.js
+++ Data/index.js
@@ -1,32 +1,40 @@
 //VARIABLES
+//LLAMADAS
+let player = new Soldier(5, 1);
+let enemy = new Zombie(4, 6);
 
-// let cuadricula = document.querySelectorAll(".celda");
-let fila = [];
-for (let i = 1; i <= 20; i++) {
-  fila.push(document.querySelector(`#row${i}>#col1`));
-}
-
-console.log(document.querySelector(`#row3>#col4`));
+let timeID = setInterval(loop, 1000);
 
 //OBJETO
-
-function Peon(/*vida, balas,*/ x, y) {
-  // this.vida = vida;
-  // this.balas = balas;
+function Soldier(x, y) {
   this.x = x;
   this.y = y;
 }
 
-// PEON
-Peon.prototype.show = function () {
+//FUNCIONES SALVAJES
+function getCasilla(col, row) {
+  return document.querySelector(`#row${row}>#col${col}`);
+}// INPUT: (x,y) OUTPUT: (Referencia a celda)
+
+window.addEventListener("keydown", (e) => {
+  player.setPos(e.key);
+}); // REVISAR? 
+
+function loop() {
+  player.setPos();
+  enemy.move();
+} //REVISAR MUY FUERTE
+
+// Soldado
+Soldier.prototype.show = function () {
   getCasilla(this.x, this.y).setAttribute("class", "soldado");
 };
 
-Peon.prototype.hide = function () {
-  // console.log(this.x, this.y)
+Soldier.prototype.hide = function () {
   getCasilla(this.x, this.y).classList.remove("soldado");
 };
-Peon.prototype.setPos = function (a) {
+
+Soldier.prototype.setPos = function (a) {
   this.hide();
   if (a === "a") {
     this.x--;
@@ -37,12 +45,11 @@ Peon.prototype.setPos = function (a) {
   } else if (a === "s") {
     this.y++;
   }
+  this.show()
 };
 
 // ZOMBIE
-function Zombie(/*vida, balas,*/ x, y) {
-  // this.vida = vida;
-  // this.balas = balas;
+function Zombie(x, y) {
   this.x = x;
   this.y = y;
 }
@@ -52,72 +59,18 @@ Zombie.prototype.show = function () {
 };
 
 Zombie.prototype.hide = function () {
-  // console.log(this.x, this.y)
   getCasilla(this.x, this.y).classList.remove("zombie");
 };
 
 Zombie.prototype.move = function () {
-  let posSoldado = document.querySelector('.soldado')
-  let posXsoldado = separarFilas(posSoldado.getAttribute('id'))
-  let posYsoldado = separarFilas(posSoldado.parentElement.getAttribute('id'))
-
-   console.log(posYsoldado)
-
-
   this.hide()
-  /*if (this.x === 1) {
-    this.x = 20;
-    this.y--;
-  } else {
-    this.x--;
-  }*/
-  if (this.x > posXsoldado) {
+  if (this.x > player.x) {
     this.x--
- } else if (this.x < posXsoldado)
-    this.x++
-    if (this.y > posYsoldado) {
-      this.y--
-    } else if (this.y < posYsoldado)
+  } else if (this.x < player.x)
+    this.x++ === null
+  if (this.y > player.y) {
+    this.y--
+  } else if (this.y < player.y)
     this.y++
   this.show()
 };
-
-//FUNCIONES SALVAJES
-function getCasilla(col, row) {
-  return document.querySelector(`#row${row}>#col${col}`);
-}
-let tecla = "d";
-tecla = window.addEventListener("keydown", (e) => {
-  //   console.log(e.key);
-  s.setPos(e.key);
-});
-
-function separarFilas(str) {
-  let s = ''
-  for (let i of str) {
-    if (i >= 0 && i <= 9)
-    s += i
-  }
-  return parseInt(s)
-}
-
-function limpiaCelda() {
-  // document.querySelectorAll("td").setAttribute("class", "celda");
-  s.setPos();
-  s.show();
-  ene1.show();
-  ene1.move();
-  // s.hide();
-}
-
-function loop() {
-  limpiaCelda();
-}
-
-///LLAMADAS
-let s = new Peon(5, 1);
-let ene1 = new Zombie(4, 6);
-
-let timeID = setInterval(loop, 1000);
-
-// console.log(document.querySelectorAll("td"));
