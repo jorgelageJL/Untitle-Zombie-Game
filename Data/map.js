59aa603a050ffcda937f46a7cff1ccb8179fb5ea
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-02-02 14:05:59 +0000

    muro añadido

--- Data/map.js
+++ Data/map.js
@@ -18,15 +18,31 @@ let map = {
   x: calculateX(),
   y: calculateY(),
   createMap: function () {
-    let row;
     const parent = document.getElementsByTagName("table")[0];
-    for (let a = 1; a <= this.x; a++) {
+    // INICIO
+    let row = document.createElement("tr");
+    row.setAttribute("id", `row1`);
+    for (let b = 1; b <= this.y; b++)
+      row.innerHTML += `<td class="wall" id="col${b}"></td>`;
+    parent.appendChild(row);
+    // MEDIO
+    for (let a = 2; a < this.x; a++) {
       row = document.createElement("tr");
       row.setAttribute("id", `row${a}`);
-      for (let b = 1; b <= this.y; b++)
+      // FIRST
+      row.innerHTML += `<td class="wall" id="col1"></td>`;
+      for (let b = 2; b < this.y; b++)
         row.innerHTML += `<td class="floor" id="col${b}"></td>`;
+      // LAST
+      row.innerHTML += `<td class="wall" id="col${this.y}"></td>`;
       parent.appendChild(row);
     }
+    // FINAL
+    row = document.createElement("tr");
+    row.setAttribute("id", `row${this.x}`);
+    for (let b = 1; b <= this.y; b++)
+      row.innerHTML += `<td class="wall" id="col${b}"></td>`;
+    parent.appendChild(row);
   },
 };
 
