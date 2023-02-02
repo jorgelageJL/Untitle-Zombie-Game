71c9858b686b8302cc1242c61de75c6134cbdafb
jorgelageJL <jorgeluis.lage@yahoo.com>
2023-02-02 16:32:49 +0000

    Boton restart funcional

--- Data/game.js
+++ Data/game.js
@@ -3,8 +3,15 @@ let created = false;
 
 playGameButton.onclick = function () {
   if (!created) {
+    playGameButton.innerText = 'Restart';
     created = true;
     map.createMap();
     loop();
+  } else {
+    playGameButton.innerText = 'Play Game';
+    let table = document.getElementsByTagName('table')[0];
+    let parent = table.parentNode;
+    parent.removeChild(table);
+    created = false;
   }
 };
