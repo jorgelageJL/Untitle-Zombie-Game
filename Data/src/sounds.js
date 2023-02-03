b0401332b104bb030e5daf12c7145c6ee7ad4562
Crayyarc <crayyarc@gmail.com>
2023-02-03 15:50:19 +0000

    sonidos

--- Data/src/sounds.js
+++ Data/src/sounds.js
@@ -1,4 +1,16 @@
+
+// VARIABLES SONIDOS
 let music = new Audio('./Data/assests/sounds/night.wav')
+
+let soundPlayer = new Audio('./Data/assests/sounds/mistery.wav')
+
+let soundSword = new Audio('./Data/assests/sounds/sword.mp3')
+let soundDamage = new Audio('./Data/assests/sounds/sword_damage.mp3')
+
+let soundvictory = new Audio('./Data/assests/sounds/victory.mp3')
+let soundgameover = new Audio('./Data/assests/sounds/mistery.wav')
+
+
 
 playGame.addEventListener("click", () => {
     music.preload = "auto";
@@ -8,9 +20,21 @@ playGame.addEventListener("click", () => {
 
 
 
-/*
 
-document.getElementById("zombie").onclick = playGameButton
+
+
+
+/*
+damage.addEventListener("click", () => {
+    soundDamage.preload = "auto";
+    soundDamage.play();
+});
+
+let soundZombie = new Audio('./Data/assests/sounds/zombie.mp3')
+let sound_Zombie = Zombie.move
+sound_Zombie.addEventListener(" ", () => {
+    soundZombie.play();
+});
 
 document.getElementById("zombie").onclick = soundZombie;
 
@@ -20,7 +44,4 @@ function soundZombie() {
     var sound_Zombie = document.getElementById("zombie");
     sound_Zombie.play("./sounds/zombie.mp3");
 }
-
-
-const soundZombie = new soundZombie("./sounds/zombie.mp3");
- /sounds/mistery.wav */
+ */
